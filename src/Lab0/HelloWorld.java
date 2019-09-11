b1af9c23817a4360a35369c3d823599f3a89ed78
James Downing <James.Downing@students.ittralee.ie>
2019-09-11 10:44:38 +0100

    Initial commit

---
+++ src/Lab0/HelloWorld.java
@@ -0,0 +1,9 @@
+package Lab0;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
---
+++ src/Lab0/HelloWorld2.java
@@ -0,0 +1,14 @@
+package Lab0;
+
+import javax.swing.*;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        //Creatic a String variable named name to store the user's input.
+        String name = JOptionPane.showInputDialog("Please enter your name: ");
+
+        JOptionPane.showMessageDialog(null,name);
+
+
+    }
+}
